5c0512491412423eebf5436ee4934b61e23cf53f
bainrachel <rachel.l.bain@gmail.com>
2022-04-16 15:45:04 -0500

    Added comments

---
+++ storm_surge_time_series.py
@@ -0,0 +1,12 @@
+"""
+Script for reading, cleaning, and plotting water level data stored in a text file.
+
+The data in this example are publicly-available water level measurements from the
+National Oceanographic and Atmospheric Administration (NOAA). However, the original
+NOAA data have been reformatted, and several errors have been added to the raw
+data file for teaching purposes.
+
+This script was written in Python 3.9.7.
+"""
+
+
